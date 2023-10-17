05ec8f631c72eb910393b1855814f50cf74e6cc5
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-10-17 19:58:08 -0300

    Aula 11

--- Aula 10/AulaAngular/src/app/calculadora/calculadora.component.ts
+++ Aula 10/AulaAngular/src/app/calculadora/calculadora.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent implements OnInit {
-  num1: number = 10;
-  num2: number = 5;
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+  operacao: string = '+';
   constructor() {}
 
   ngOnInit(): void {}
+
+  calcular() {
+    if (this.operacao == '+') this.resultado = this.num1 + this.num2;
+    else this.resultado = this.num1 - this.num2;
+  }
 }
